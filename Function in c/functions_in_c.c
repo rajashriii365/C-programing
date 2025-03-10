cc80a883cd86bbf5a3c79495469a934deb2cbeb9
rajashriii365 <rajashriiisonwalkar@gmail.com>
2025-03-10 23:38:20 +0530

    create function.c

---
+++ Function in c/functions_in_c.c
@@ -0,0 +1,74 @@
+// header file
+#include<stdio.h>
+
+// function prototype/Declareation
+int addition();
+float subtration();
+int multi();
+double div();
+
+// main function/ function calling
+int main(){
+	addition();
+	subtration();
+	multi();
+	div();
+}
+
+// function defination
+// addition
+int addition()
+{
+	printf("addition :\n");
+	int a, b;
+	printf("Enter A:\n");
+	scanf("%d",&a);
+	printf("Enter B:\n");
+	scanf("%d", &b);
+	
+	int result = a + b;
+	printf("%d\n", result);
+}
+
+// Subtrcation
+float subtration()
+{
+	printf("\nSub :");
+	float a, b;
+	printf("Enter A:\n");
+	scanf("%f",&a);
+	printf("Enter B:\n");
+	scanf("%f", &b);
+	
+	float result = a - b;
+	printf("%f\n", result);
+}
+
+// multi
+int multi()
+{
+	printf("Multi :\n");
+	int a, b;
+	printf("Enter A:\n");
+	scanf("%d",&a);
+	printf("Enter B:\n");
+	scanf("%d", &b);
+	
+	int result = a * b;
+	printf("%d\n", result);
+}
+
+// div
+double div()
+{
+	printf("Div :\n");
+	double a, b;
+	printf("Enter A:\n");
+	scanf("%lf",&a);
+	printf("Enter B:\n");
+	scanf("%lf", &b);
+	
+	printf("a/b : %lf\n", a/b);
+	double result = b / a;
+	printf("b/a : %lf\n", result);
+}
